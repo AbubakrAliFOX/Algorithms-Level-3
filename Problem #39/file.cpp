a25df5848fe27fe2fa6fe091b8818ac68edcc666
Abubakr <thefox5000@gmail.com>
2024-01-14 09:15:04 +0300

    Join strings with deliminer

---
+++ Problem #39/file.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+
+using namespace std;
+string ReadString()
+{
+    string Str;
+    cout << "Please Enter Your String?\n";
+    getline(cin, Str);
+    return Str;
+}
+
+short CountWords(string Str)
+{
+    string delim = " "; // delimiter
+    cout << "\nYour string wrords are: \n\n";
+    short pos = 0;
+    string sWord; // define a string variable  // use find() function to get the position of the delimiters
+
+    short Counter = 0;
+    while ((pos = Str.find(delim)) != std::string::npos)
+    {
+        sWord = Str.substr(0, pos); // store the word
+        if (sWord != "")
+        {
+            cout << sWord << endl;
+            Counter++;
+        }
+        Str.erase(0, pos + delim.length()); /* erase() until positon and move to next word. */
+    }
+    if (Str != "")
+    {
+        cout << Str << endl; // it print last word of the string.
+        Counter++;
+    }
+
+    return Counter;
+}
+
+string join(vector<string> vWords, string Delim)
+{
+    string Str;
+    for (short i = 0; i < vWords.size(); i++)
+    {
+        Str = Str + vWords[i] + Delim;
+    }
+
+    return Str.substr(0, Str.length() - Delim.length());
+}
+
+int main()
+{
+    vector<string> vWords = {"Ahmad", "Khaled", "Menza"};
+
+    cout << join(vWords, " || ") << endl;
+    return 0;
+}
