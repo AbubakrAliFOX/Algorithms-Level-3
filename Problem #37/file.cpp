68f91f63fade23c7eb89de51189ec79125186075
Abubakr <thefox5000@gmail.com>
2024-01-14 08:45:15 +0300

    String split function

--- Problem #37/file.cpp
+++ Problem #37/file.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <vector>
 
 using namespace std;
 string ReadString()
 {
-    string S1;
+    string Str;
     cout << "Please Enter Your String?\n";
-    getline(cin, S1);
-    return S1;
+    getline(cin, Str);
+    return Str;
 }
 
-short CountWords(string S1)
+short CountWords(string Str)
 {
     string delim = " "; // delimiter
     cout << "\nYour string wrords are: \n\n";
@@ -19,26 +20,59 @@ short CountWords(string S1)
     string sWord; // define a string variable  // use find() function to get the position of the delimiters
     
     short Counter = 0;
-    while ((pos = S1.find(delim)) != std::string::npos)
+    while ((pos = Str.find(delim)) != std::string::npos)
     {
-        sWord = S1.substr(0, pos); // store the word
+        sWord = Str.substr(0, pos); // store the word
         if (sWord != "")
         {
             cout << sWord << endl;
             Counter ++;
         }
-        S1.erase(0, pos + delim.length()); /* erase() until positon and move to next word. */
+        Str.erase(0, pos + delim.length()); /* erase() until positon and move to next word. */
     }
-    if (S1 != "")
+    if (Str != "")
     {
-        cout << S1 << endl; // it print last word of the string.
+        cout << Str << endl; // it print last word of the string.
         Counter++;
     }
-
+ 
     return Counter;
+}
+
+vector<string> split(string Str, string Delim) {
+
+    short pos = 0;
+    string sWord; // define a string variable  // use find() function to get the position of the delimiters
+    short Counter = 0;
+    vector<string> vWords;
+    while ((pos = Str.find(Delim)) != std::string::npos)
+    {
+        sWord = Str.substr(0, pos); // store the word
+        if (sWord != "")
+        {
+            vWords.push_back(sWord);
+            Counter ++;
+        }
+        Str.erase(0, pos + Delim.length()); /* erase() until positon and move to next word. */
+    }
+    if (Str != "")
+    {
+        vWords.push_back(Str); // it print last word of the string.
+        Counter++;
+    }
+ 
+    return vWords;
+}
+
+void PrintVectorTokens(vector<string> vWords) {
+    for (string &item : vWords)
+    {
+        cout << item << endl;
+    }
+    
 }
 
 int main()
 {
-    cout << CountWords(ReadString()) << endl;
+    PrintVectorTokens(split(ReadString(), "v"));
 }
